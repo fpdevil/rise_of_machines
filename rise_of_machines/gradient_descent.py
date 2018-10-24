31791eb51d1a11a8b06c7e69aa343c1a1a468fc0
fpdevil <singamsetty.sampath@gmail.com>
2018-10-24 12:22:32 +0530

    updated comments and headers

--- rise_of_machines/gradient_descent.py
+++ rise_of_machines/gradient_descent.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright (C) 2018
 
-# Author       : Sampath Singamsetty
-# Created Time : Sat Oct  6 22:06:47 2018
-# File Name    : gradient_descent.py
-# Description  : Using Gradient descent in Linear regression
-########################################################################
+"""
 
-#  An example of  modelling a line commanded by  y = mx +c through a  set of points
-#  with m = slope and c= y-intercept.The goal  is to find the best values for m and
-#  c. A  standard approach to solving  this type of  problem is to define  an error
-#  function (also called a cost function) that measures how “good” a given line is.
-#  This function will take  in a (m,c) pair and return an error  value based on how
-#  well the  line fits  our data.  To compute this  error for  a given  line, we’ll
-#  iterate through each  (x,y) point in our  data set and sum  the square distances
-#  between each point’s y value and the  candidate line’s y value (computed at mx +
-#  c). It’s conventional to square this distance  to ensure that it is positive and
-#  to make our error function differentiable.
+Gradient Descent
+-----------------
+
+Gradient Descent using Linear regression
+
+An example of  modelling a line commanded by  y = mx +c through a  set of points
+with m = slope and c= y-intercept.The goal  is to find the best values for m and
+c. A  standard approach to solving  this type of  problem is to define  an error
+function (also called a cost function) that measures how “good” a given line is.
+This function will take  in a (m,c) pair and return an error  value based on how
+well the  line fits  our data.  To compute this  error for  a given  line, we’ll
+iterate through each  (x,y) point in our  data set and sum  the square distances
+between each point’s y value and the  candidate line’s y value (computed at mx +
+c). It’s conventional to square this distance  to ensure that it is positive and
+to make our error function differentiable.
+
+Authors: Sampath Singamsetty
+
+:module: gradient_descent
+:created: Sat Oct  6 22:06:47 2018
+:platform: OS X
+:copyright: Copyright © 2018 Sampath Singamsetty
+:license: MIT, see LICENSE for details.
+:moduleauthor: Sampath Singamsetty <Singamsetty.Sampath@gmail.com>
+"""
 
 def get_error(intercept, slope, points):
     """get_error function computes the error for a line passing through a
