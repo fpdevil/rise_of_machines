3b933a06130e603414fa340fd2f39172f24fa858
fpdevil <singamsetty.sampath@gmail.com>
2019-02-02 12:52:57 -0600

    updated sigmoid

--- rise_of_machines/sigmoid.py
+++ rise_of_machines/sigmoid.py
@@ -32,20 +32,31 @@ def sigmoid(z):
 # plotting the sigmoid
 x = np.arange(-10., 10., 0.1)
 s = sigmoid(x)
+
 plt.plot(x, s)
-plt.grid()
+plt.grid(True)
 
 plt.rc('axes', labelsize=14)
 plt.rc('font', size=14)
 
-plt.title('Logistic Sigmoid')
+plt.title("Logistic Sigmoid Activation function", fontsize=14)
 plt.xlabel('z')
-plt.ylabel('$\phi$(z)')
+plt.ylabel(r'$\phi$(z)')
 
 # set some math info on the graph
 plt.text(-9.0, 0.85, r'$\phi(z) = \frac{1}{1 + e^{-z}}$')
+
+# text and annotation
+props = dict(facecolor='black', shrink=0.1)
 plt.text(-9.0, 0.70, r'$z \rightarrow +\infty \Leftrightarrow \phi(z) \rightarrow 1$')
 plt.text(-9.0, 0.60, r'$z \rightarrow -\infty \Leftrightarrow \phi(z) \rightarrow 0$')
+
+plt.annotate('Saturating', xytext=(4.5, 0.8), xy=(10, 1),
+             arrowprops=props, fontsize=14, ha="center")
+plt.annotate('Saturating', xytext=(-4.5, 0.2), xy=(-10, 0),
+             arrowprops=props, fontsize=14, ha="center")
+plt.annotate('Linear', xytext=(3, 0.3), xy=(0, 0.5),
+             arrowprops=props, fontsize=14, ha="center")
 
 # draw dotted lines crossing at (0, 0.5)
 plt.axvline(linewidth=2, color='g', linestyle='dashed')
