3fc8e280ff544931290cdf586223d78b0e15cb60
fpdevil <singamsetty.sampath@gmail.com>
2020-08-09 02:39:18 -0500

    updated exported functions

--- rise_of_machines/__init__.py
+++ rise_of_machines/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+from helpers import plot_classifier, draw_decision_boundary
 
 __author__ = """Sampath Singamsetty"""
 __email__ = 'Singamsetty.Sampath@gmail.com'
